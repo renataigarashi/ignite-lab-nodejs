5da56eb3a465ce7b8a4063f8bf1b5a0a258c0445
Renata Igarashi <renata.sayuri@dasa.com.br>
2022-12-13 19:12:26 -0300

    initial config

---
+++ src/create-notification-body.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 240) // primeiro parametro min, segundo max
+  content: string;
+
+  @IsNotEmpty()
+  category: string;
+}
